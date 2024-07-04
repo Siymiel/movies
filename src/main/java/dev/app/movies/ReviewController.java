4fbb3d08bbbe2f927df0210e03076307973ad909
Siymiel <skinuthia77@gmail.com>
2024-07-04 10:33:52 +0300

    updates on reviews files

--- src/main/java/dev/app/movies/ReviewController.java
+++ src/main/java/dev/app/movies/ReviewController.java
@@ -1,14 +1,8 @@
 package dev.app.movies;
 
-import java.util.List;
-import java.util.Optional;
-
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,15 +17,5 @@ public class ReviewController {
     @PostMapping
     public ResponseEntity<Review> createReview(@RequestBody java.util.Map<String, String> payload) {
         return new ResponseEntity<Review>(reviewService.createReview(payload.get("reviewBody"), payload.get("imdbId")), HttpStatus.CREATED);
-    }
-
-    @GetMapping
-    public ResponseEntity<List<Review>> getReviews() {
-        return new ResponseEntity<List<Review>>(reviewService.getReviews(), HttpStatus.OK);
-    }
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Optional<Review>> getReviewById(@PathVariable ObjectId id) {
-        return new ResponseEntity<Optional<Review>>(reviewService.getReviewById(id), HttpStatus.OK);
     }
 }
